d90775eed9ad8fe3930e635c5d0974271c893646
devruhul <ruhulamin262001@gmail.com>
2022-05-26 13:39:02 +0600

    comment menu item cause it's not installed

---
+++ src/pages/Home/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../../Shared/Header/Header';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>This is Home</h2>
+            <Header />
+        </div>
+    );
+};
+
+export default Home;
