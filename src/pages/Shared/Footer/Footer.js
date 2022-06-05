461cbd2506430a61a897be946fefa6e3e05c532c
devruhul <ruhulamin262001@gmail.com>
2022-06-06 00:38:25 +0600

    added footer page and bg image

--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -3,16 +3,23 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import footerbg from '../../../images/footer-bg.png'
+
+const footerBg = {
+    backgroundImage: `url(${footerbg})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+}
 
 const Footer = () => {
     return (
         //    create a footer with material ui
-        <Box sx={{ mt: 10, mb: 10, p: 10, backgroundColor: '#5CE7ED' }}>
-            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} spacing={0}>
-                <Grid item sx={{ mt: 4, textAlign: 'left' }} xs={12} md={3} sm={6}>
-                    <Typopgraphy variant="h6" gutterBottom>
-                        Oral Health
-                    </Typopgraphy>
+        <Box style={footerBg} sx={{ mt: 10, mb: 10, p: 10 }}>
+            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} spacing={5}>
+                <Grid container direction="column"
+                    justifyContent="space-between"
+                    alignItems="center" item sx={{ mt: 4 }} xs={12} md={3} sm={6}>
                     <Typography variant="caption" sx={{ color: 'black' }}>
                         Emergency Dental Care
                     </Typography>
@@ -28,36 +35,13 @@ const Footer = () => {
                     <Typography variant="caption" sx={{ color: 'black' }}>
                         Cleaning Teeth
                     </Typography>
-
                 </Grid>
-                <Grid item sx={{ textAlign: 'left' }} xs={12} md={3} sm={6}>
-                    <Typopgraphy variant="h6" gutterBottom>
+                <Grid container direction="column"
+                    justifyContent="space-between"
+                    alignItems="center" item xs={12} md={3} sm={6}>
+                    <Typography variant="h6" gutterBottom>
                         Services
-                    </Typopgraphy>
-                    <Typopgraphy variant="h6" gutterBottom>
-                        Oral Health
-                    </Typopgraphy>
-                    <Typography variant="caption" sx={{ color: 'black' }}>
-                        Emergency Dental Care
                     </Typography>
-                    <Typography variant="caption" sx={{ color: 'black' }}>
-                        Check Up
-                    </Typography>
-                    <Typography variant="caption" sx={{ color: 'black' }}>
-                        Treatment of Personal Diseases
-                    </Typography>
-                    <Typography variant="caption" sx={{ color: 'black' }}>
-                        Tooth Extraction
-                    </Typography>
-                    <Typography variant="caption" sx={{ color: 'black' }}>
-                        Cleaning Teeth
-                    </Typography>
-
-                </Grid>
-                <Grid item sx={{ textAlign: 'left' }} xs={12} md={3} sm={6}>
-                    <Typopgraphy variant="h6" gutterBottom>
-                        Oral Health
-                    </Typopgraphy>
                     <Typography variant="caption" sx={{ color: 'black' }}>
                         Emergency Dental Care
                     </Typography>
@@ -74,30 +58,52 @@ const Footer = () => {
                         Cleaning Teeth
                     </Typography>
                 </Grid>
-                <Grid item xs={12} md={3} sm={6}>
-                    <Typopgraphy sx={{ color: '#5CE7ED' }} variant="h6" gutterBottom>
+                <Grid container direction="column"
+                    justifyContent="space-between"
+                    alignItems="center" item xs={12} md={3} sm={6}>
+                    <Typography variant="h6" gutterBottom>
+                        Oral Health
+                    </Typography>
+                    <Typography variant="caption" sx={{ color: 'black' }}>
+                        Emergency Dental Care
+                    </Typography>
+                    <Typography variant="caption" sx={{ color: 'black' }}>
+                        Check Up
+                    </Typography>
+                    <Typography variant="caption" sx={{ color: 'black' }}>
+                        Treatment of Personal Diseases
+                    </Typography>
+                    <Typography variant="caption" sx={{ color: 'black' }}>
+                        Tooth Extraction
+                    </Typography>
+                    <Typography variant="caption" sx={{ color: 'black' }}>
+                        Cleaning Teeth
+                    </Typography>
+                </Grid>
+                <Grid container direction="column"
+                    justifyContent="space-between"
+                    alignItems="center" item xs={12} md={3} sm={6}>
+                    <Typography sx={{ color: '#5CE7ED' }} variant="h6" gutterBottom>
                         Our Address
-                    </Typopgraphy>
-                    <Typopgraphy variant="caption" gutterBottom>
-
+                    </Typography>
+                    <Typography variant="caption" gutterBottom>
                         Birmingham - UK, B15 2JX
-                    </Typopgraphy>
+                    </Typography>
                     {/* facebook google twitter icon */}
-                    <Box sx={{ mt: 1 }}>
+                    <Box sx={{ mt: 1, my: 3 }}>
                         <i className="fa-brands fa-facebook-f fa-2x"></i>
                         <i className="fa-brands fa-google fa-2x"></i>
                         <i className="fa-brands fa-twitter fa-2x"></i>
                     </Box>
-
-                    <Typopgraphy variant="body2" gutterBottom>
+                    <Typography variant="body2" gutterBottom>
                         Call Now
-                    </Typopgraphy>
-                    <Button variant="contained">
-                        +8801165146514
+                    </Typography>
+                    <Button variant='contained'>
+                        <a style={{ textDecoration: 'none', color: 'white' }} href="tel:+8801764896656">01764896656</a>
                     </Button>
                 </Grid>
             </Grid>
-        </Box>
+        </Box >
     );
 };
 
