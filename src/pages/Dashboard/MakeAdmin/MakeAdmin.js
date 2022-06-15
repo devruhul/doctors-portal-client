5b2b83d83899e313654339181c8c518cd6e5659d
devruhul <ruhulamin262001@gmail.com>
2022-06-15 16:02:38 +0600

    added make admin page adn send data to the database

--- src/pages/Dashboard/MakeAdmin/MakeAdmin.js
+++ src/pages/Dashboard/MakeAdmin/MakeAdmin.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import { Box, Typography, TextField, Button } from '@mui/material';
+import React, { useState } from 'react';
 
 const MakeAdmin = () => {
+    const [email, setEmail] = useState('');
+    const handleOnBlur = (e) => {
+        setEmail(e.target.value);
+    }
+
+    const handleMakeAdmin = (e) => {
+        e.preventDefault();
+        const user = { email };
+        fetch('http://localhost:5000/users/makeAdmin', {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount < 0) {
+                    alert('admin added succesfully')
+                    e.target.reset();
+                } else {
+                    alert('admin existed')
+                }
+            })
+    }
+
     return (
-        <div>
-            <h2>Make an admin</h2>
-        </div>
+        <Box>
+            <Typography variant="h4">Make an admin</Typography> <br />
+            <form onSubmit={handleMakeAdmin}>
+                <TextField
+                    id="standard-basic"
+                    onBlur={handleOnBlur}
+                    label="Email"
+                    name="email"
+                    variant="standard" /> <br /> <br />
+                <Button variant="contained" color="primary" type="submit">
+                    Make Admin
+                </Button>
+            </form>
+
+        </Box>
     );
 };
 
