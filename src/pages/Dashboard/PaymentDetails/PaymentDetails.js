eb1b34772f4d1fc7e36a6b8dc984eb709cb6dd16
devruhul <ruhulamin262001@gmail.com>
2022-08-29 16:42:03 +0600

    feat: added stripe payment

--- src/pages/Dashboard/PaymentDetails/PaymentDetails.js
+++ src/pages/Dashboard/PaymentDetails/PaymentDetails.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import CheckoutForm from './CheckoutForm';
+
+const stripePromise = loadStripe('pk_test_51LbhEkE2qPyb4aLsG8XuYzxI0q1WCnVgqsWDPT7UXBlNOEsN328Q1GjKBXEDMq62NzzjDp8tFjJVI3DgibrIvN1s00KecszPMl');
 
 const PaymentDetails = () => {
-    const { paymentId } = useParams()
+    const { paymentId } = useParams();
+    const [appointment, setAppointment] = useState({});
+    useEffect(() => {
+        fetch(`http://localhost:5000/appointments/${paymentId}`)
+            .then(res => res.json())
+            .then(data => setAppointment(data));
+    }, [paymentId]);
     return (
         <div>
-            <h2>Payment for: {paymentId}</h2>
+            <h2>Please Pay for: {appointment.patientName} for {appointment.serviceName}</h2>
+            <h4>Pay:{appointment.price}</h4>
+            {appointment?.price && <Elements stripe={stripePromise}>
+                <CheckoutForm
+                    appointment={appointment}
+                />
+            </Elements>}
         </div>
     );
 };
